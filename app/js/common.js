6dac522da1f03b316edf576fc2e0c0ebac5e59a2
VjacheslavZ <slavikov.net@mail.ru>
2017-12-05 04:49:25 +0200

    some bugs

--- app/js/common.js
+++ app/js/common.js
@@ -13,6 +13,7 @@
         //здесь события
         this.getPlayField(self);
         this.createPlayField(self);
+        this.createRandomPictures(self)
     };
 
     PlayGame.prototype.getPlayField = function (self) {
@@ -44,15 +45,48 @@
 
                     let row = document.querySelector(self._settings.rowAreaElem);
 
-                    row.insertAdjacentHTML("afterbegin", `<th><img src="img/logo.png"></th>`);
+                    row.insertAdjacentHTML("afterbegin", `<th><img src=""></th>`);
 
                 }
             }
-            console.log(self._settings.fields)
+            // console.log(self._settings.fields)
         },500)
     };
 
-    
+
+    PlayGame.prototype.createRandomPictures = function (self) {
+        setTimeout(function () {
+
+            let evenPictures = (self._settings.fields.height * self._settings.fields.width)/2,
+                arrPictures= [],
+                size = 0;
+
+            for (key in self._settings.urlImages){
+
+                arrPictures.push(self._settings.urlImages[key]);
+                size++
+            }
+
+            if(evenPictures > size){
+                for (var i = 0, max = evenPictures - size; i < max; i++){
+                    arrPictures.push(arrPictures[i])
+                }
+            }
+
+            console.log(arrPictures);
+
+            let images = document.querySelectorAll("img");
+            
+            for (var i =0, max = images.length; i < max; i++){
+                images[i].setAttribute("src", arrPictures[i]);
+            }
+
+
+
+        },500)
+    };
+
+
     //параметры игры
     let param = new PlayGame({
         urlFields: "https://kde.link/test/get_field_size.php",
@@ -62,19 +96,20 @@
         urlImages: {
             image1:"https://kde.link/test/1.png",
             image2:"https://kde.link/test/2.png",
-            image3:"https://kde.link/test/9.png",
-            image4:"https://kde.link/test/7.png",
-            image5:"https://kde.link/test/6.png",
-            image6:"https://kde.link/test/3.png",
-            image7:"https://kde.link/test/4.png",
-            image8:"https://kde.link/test/0.png",
-            image9:"https://kde.link/test/5.png",
-            image10:"https://kde.link/test/8.png"
+            image3:"https://kde.link/test/3.png",
+            image4:"https://kde.link/test/4.png",
+            image5:"https://kde.link/test/5.png",
+            image6:"https://kde.link/test/6.png",
+            image7:"https://kde.link/test/7.png",
+            image8:"https://kde.link/test/8.png",
+            image9:"https://kde.link/test/9.png",
+            image10:"https://kde.link/test/0.png"
         },
 
         playAreaElem: "table tbody",
         rowAreaElem: "tr",
         columnAreaElem: "th",
+        identicalPictures: 2
 
     });
 
