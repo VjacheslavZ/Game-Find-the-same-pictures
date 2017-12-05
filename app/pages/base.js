6a23ee13e700fa3247822d35caf36e2e3dc07b78
VjacheslavZ <slavikov.net@mail.ru>
2017-12-05 20:07:15 +0200

    function createRandomPictures is finished

--- app/pages/base.js
+++ app/pages/base.js
@@ -23,12 +23,16 @@
             if (this.readyState == 4 && this.status == 200) {
 
                 let data = JSON.parse(this.responseText);
+
+                console.log(data)
+
                 return self._settings.fields = data;
             }
         };
 
         xmlhttp.open("GET", self._settings.urlFields, true);
         xmlhttp.send();
+
 
     };
 
@@ -49,7 +53,6 @@
 
                 }
             }
-            // console.log(self._settings.fields)
         },500)
     };
 
@@ -57,31 +60,36 @@
     PlayGame.prototype.createRandomPictures = function (self) {
         setTimeout(function () {
 
-            let evenPictures = (self._settings.fields.height * self._settings.fields.width)/2,
+            let cells = (self._settings.fields.height * self._settings.fields.width),
                 arrPictures= [],
-                size = 0;
+                size = 0,
+                iterations = Math.ceil((cells - size)/10);
+
+                console.log(size)
+
+            if(iterations & 1) iterations++;
 
             for (key in self._settings.urlImages){
+                size++;
 
-                arrPictures.push(self._settings.urlImages[key]);
-                size++
-            }
-
-            if(evenPictures > size){
-                for (var i = 0, max = evenPictures - size; i < max; i++){
-                    arrPictures.push(arrPictures[i])
+                for(let i = 0,max = iterations; i < max; i++){
+                    arrPictures.push(self._settings.urlImages[key])
                 }
             }
 
-            console.log(arrPictures);
+            //cut arr
+            arrPictures.length = cells;
+            function rand() {
+                return Math.ceil(Math.random() * (arrPictures.length - 0) + 0);
+            }
+
+            arrPictures.sort(rand);
 
             let images = document.querySelectorAll("img");
-            
+
             for (var i =0, max = images.length; i < max; i++){
                 images[i].setAttribute("src", arrPictures[i]);
             }
-
-
 
         },500)
     };
@@ -94,6 +102,7 @@
         activeClass: 'selected',
 
         urlImages: {
+
             image1:"https://kde.link/test/1.png",
             image2:"https://kde.link/test/2.png",
             image3:"https://kde.link/test/3.png",
@@ -103,13 +112,19 @@
             image7:"https://kde.link/test/7.png",
             image8:"https://kde.link/test/8.png",
             image9:"https://kde.link/test/9.png",
-            image10:"https://kde.link/test/0.png"
+            image10:"https://kde.link/test/0.png",
+
+
         },
 
         playAreaElem: "table tbody",
         rowAreaElem: "tr",
         columnAreaElem: "th",
-        identicalPictures: 2
+        identicalPictures: 2,
+        // fields:{
+        //     width: 8,
+        //     height:8
+        // }
 
     });
 
